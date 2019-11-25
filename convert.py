7643f2ce9fd80e5084414ed2e13f9868c8200a83
Angelo Laub <angelolaub@gmail.com>
2019-11-25 12:56:29 +0100

    :wq

--- convert.py
+++ convert.py
@@ -1,22 +1,19 @@
 import csv
 import sys
+import io
 
 def main():
     try:
         filename = sys.argv[1]
-        csvfile = open(filename)
+        csvfile = io.open(filename, encoding="utf-8")
     except IndexError:
         print("Error: Please give a filename to convert as first argument")
         sys.exit(1)
     
-    dialect = csv.Sniffer().sniff(csvfile.read(2048))
-    csvfile.seek(0)
-    
-    outfile = open(filename + '_converted.csv', 'w')
+    outfile = io.open(filename + '_converted.csv', 'w', encoding="utf-8")
     writer = csv.writer(outfile, delimiter=',', quoting=csv.QUOTE_ALL)
-    for line in csv.reader(csvfile, dialect):
+    for line in csv.reader(csvfile, delimiter=','):
         writer.writerow(line)
-    
     
     
 if __name__ == '__main__':
